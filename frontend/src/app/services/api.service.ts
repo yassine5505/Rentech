fd5bcc3179936baf04e60974849386c78b81ec6d
Valdor <valdesche03@gmail.com>
2019-04-11 17:28:29 +0100

    Adding ApiService and Init User Service

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable ,  throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+  get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
+    return this.http.get(`${environment.api_url}${path}`, { params });
+  }
+
+  put(path: string, body: object = {}) {
+    return this.http.put(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body));
+  }
+
+  post(path: string, body: object = {}): Observable<any> {
+    return this.http.post(
+      `${environment.api_url}${path}`,
+      JSON.stringify(body));
+  }
+
+  delete(path): Observable<any> {
+    return this.http.delete(
+      `${environment.api_url}${path}`);
+  }
+}
--- frontend/src/app/services/authentication/authentication.service.ts
+++ frontend/src/app/services/authentication/authentication.service.ts
@@ -1,34 +1,33 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+import { environment } from '../../../environments/environment';
 @Injectable()
 export class AuthenticationService {
-  private baseUrl = 'http://127.0.0.1:8000/api';
 
   constructor(private http: HttpClient) { }
 
   signup(data) {
-    return this.http.post(`${this.baseUrl}/signup`, data);
+    return this.http.post(`${environment.api_url}/signup`, data);
   }
 
   login(data) {
-    return this.http.post(`${this.baseUrl}/login`, data);
+    return this.http.post(`${environment.api_url}/login`, data);
   }
 
   logout() {
-    return this.http.post(`${this.baseUrl}/logout`, {});
+    return this.http.post(`${environment.api_url}/logout`, {});
   }
 
   sendPasswordResetLink(data) {
-    return this.http.post(`${this.baseUrl}/sendPasswordResetLink`, data);
+    return this.http.post(`${environment.api_url}/sendPasswordResetLink`, data);
   }
 
   changePassword(data) {
-    return this.http.post(`${this.baseUrl}/resetPassword`, data);
+    return this.http.post(`${environment.api_url}/resetPassword`, data);
   }
 
   me() {
-    return this.http.post(`${this.baseUrl}/me`, {});
+    return this.http.post(`${environment.api_url}/me`, {});
   }
 
 }
