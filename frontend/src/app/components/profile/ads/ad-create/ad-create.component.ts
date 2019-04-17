0ddae1b28989aad243b720e92598c2eb9c62e00d
Valdor <valdesche03@gmail.com>
2019-04-17 23:12:39 +0100

    Ad created succesfully

--- frontend/src/app/components/profile/ads/ad-create/ad-create.component.ts
+++ frontend/src/app/components/profile/ads/ad-create/ad-create.component.ts
@@ -7,7 +7,7 @@ import { CityService } from './../../../../services/shared/city/city.service';
 import { City } from './../../../../models/city.model';
 import { Subscription } from 'rxjs';
 import { AdService } from './../../../../services/shared/ad/ad.service';
-import { Car } from 'src/app/models/car.model';
+import { Car } from './../../../../models/car.model';
 import { DatePipe } from '@angular/common';
 @Component({
   selector: 'app-ad-create',
@@ -15,14 +15,14 @@ import { DatePipe } from '@angular/common';
   styleUrls: ['./ad-create.component.scss'],
   providers: [DatePipe]
 })
-export class AdCreateComponent implements OnInit,  OnDestroy {
+export class AdCreateComponent implements OnInit, OnDestroy {
   newAdForm: FormGroup;
   submitted = false;
   error: string[] = [];
   success = null;
   cities: City[] = [];
   citySubscription: Subscription;
-  selectedCar: Car = null ;
+  selectedCar: Car = null;
   allMyCars: Car[] = [];
   constructor(
     private router: Router,
@@ -37,6 +37,27 @@ export class AdCreateComponent implements OnInit,  OnDestroy {
 
   ngOnInit() {
     this.loaderService.startLoading();
+    this.newAdForm = this.formBuilder.group(
+      {
+        city_id: ['', Validators.required],
+        start_date: ['', Validators.required],
+        end_date: ['', Validators.required],
+        description: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(5)
+          ]
+        ],
+        price: [100, [
+          Validators.required,
+          Validators.min(100),
+          Validators.max(2000)
+        ]],
+        car_id: [null, Validators.required],
+        status: [true, null]
+      }, { validator: this.dateLessThan('start_date', 'end_date') }
+    );
     this.citySubscription = this.cityService.getAll().subscribe(
       (data) => {
         this.cities = data;
@@ -53,10 +74,12 @@ export class AdCreateComponent implements OnInit,  OnDestroy {
         this.carService.get(carId).subscribe(
           (successData) => {
             this.selectedCar = successData;
+            this.newAdForm.patchValue({car_id: this.selectedCar.id});
           },
           (error) => {
             this.selectedCar = null;
-          }
+          },
+          () => this.loaderService.stopLoading()
         );
       }
     });
@@ -71,33 +94,14 @@ export class AdCreateComponent implements OnInit,  OnDestroy {
         (error) => {
           this.error.push('Aucune voiture trouvée , donc impossible de créer une offre !');
           this.loaderService.stopLoading();
-        }
+        },
+        () => this.loaderService.stopLoading()
       );
     }
 
-    this.newAdForm = this.formBuilder.group(
-      {
-        city_id: ['', Validators.required],
-        date_start: ['', Validators.required],
-        date_end: ['', Validators.required],
-        description: [
-          '',
-          [
-            Validators.required,
-            Validators.minLength(5)
-          ]
-        ],
-        price: [100, [
-          Validators.required,
-          Validators.min(100),
-          Validators.max(2000)
-        ]],
-        car_id: [null, null]
-      }, { validator: this.dateLessThan('date_start', 'date_end') }
-    );
   }
   dateLessThan(from: string, to: string) {
-    return (group: FormGroup): {[key: string]: any} => {
+    return (group: FormGroup): { [key: string]: any } => {
       const f = group.controls[from];
       const t = group.controls[to];
       if (f.value < new Date()) {
@@ -112,7 +116,7 @@ export class AdCreateComponent implements OnInit,  OnDestroy {
       }
       return {};
     };
-}
+  }
   onSubmit() {
     this.submitted = true;
     this.loaderService.startLoading();
@@ -122,10 +126,19 @@ export class AdCreateComponent implements OnInit,  OnDestroy {
       this.error.push('Certaines informations du formulaire sont invalides !');
       return;
     }
+    this.newAdForm.patchValue(
+      {
+        start_date:
+        this.datePipe.transform(this.newAdForm.value.start_date, 'yyyy-MM-dd'),
+        end_date:
+        this.datePipe.transform(this.newAdForm.value.end_date, 'yyyy-MM-dd') ,
+      }
+    );
 
     this.adService.add(this.newAdForm.value).subscribe(
       data => {
         this.handleResponse(data);
+        this.newAdForm.reset();
       },
       error => {
         this.handleError(error);
