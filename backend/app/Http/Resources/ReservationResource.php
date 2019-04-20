831c8c080205c5c33c1ac5426c1797c37018de06
Mohammed Yassine Chraibi <medyassinechraibi@gmail.com>
2019-04-20 21:04:05 +0100

    Added Reservation API resources and modified reservation relationships with user and ad models

---
+++ backend/app/Http/Resources/ReservationResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Htp\Resources\AdResource;
+use App\Htp\Resources\UserResource;
+class ReservationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'status' -> $this->status,
+            'created_at' -> $this->created_at,
+            'updated_at' -> $this->updated_at,
+            'ad' => new AdResource($this->ad),
+            'reservator' => new UserResource($this->reservator)
+        ];
+    }
+}
