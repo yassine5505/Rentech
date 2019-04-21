2aeec8730a80ec40874be6c0fc641536817e59fc
Valdor <valdesche03@gmail.com>
2019-04-21 12:01:21 +0100

    Filter menu designed with button click interractions

--- frontend/src/app/components/ads-page/ads-page.component.ts
+++ frontend/src/app/components/ads-page/ads-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-ads-page',
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ads-page.component.scss']
 })
 export class AdsPageComponent implements OnInit {
+  // Reference to filter box Component
+  @ViewChild('filterBox') filterBox: ElementRef;
+
+  fromMinimun = new Date().toLocaleDateString();
 
   constructor() { }
 
   ngOnInit() {
+    this.fromMinimun = new Date().toLocaleDateString();
+  }
+
+
+  toggleFilter() {
+    const filter: HTMLElement = this.filterBox.nativeElement;
+    const classList = filter.classList;
+    if (classList.contains('filter-is-visible')) {
+      filter.classList.remove('filter-is-visible');
+      return;
+    }
+    filter.classList.add('filter-is-visible');
   }
 
 }
