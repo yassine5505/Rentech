f4e21913b97c6254730d0427d1af7cbde8228dc5
Valdor <valdesche03@gmail.com>
2019-05-13 14:14:46 +0000

    Solving undefined image issue on ad item

--- backend/database/factories/CitiesFactory.php
+++ backend/database/factories/CitiesFactory.php
@@ -5,6 +5,6 @@ use Faker\Generator as Faker;
 $factory->define(App\City::class, function (Faker $faker) {
     return [
         'name' => $faker->city(),
-        'description' => $faker->text() ,
+        'description' => $faker->name,
     ];
 });
